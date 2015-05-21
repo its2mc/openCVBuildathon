20d868e5c0259686dca0711450154b04acf532af
its2mc <its2uraps@msn.com>
2015-05-21 09:18:03 +0300

    Added the nodejs express server

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # openCVBuildathon
 Support files for the intel edison opencv Buildathon
+
+
+To run the nodejs script in the background run "node stream.js &"
+Make sure you record the pid number
---
+++ stream.js
@@ -0,0 +1,21 @@
+/*
+* Disclaimer and other stuff
+*/
+
+//Nodejs server for edison stream processor
+
+var express = require('express'),
+	path = require('path'),
+	httpPort = 800,
+	app = express();
+	
+	
+//Set all environments
+express.static.mime.default_type = "text/html"
+ 
+app.use(express.static(path.join(__dirname, 'static')))
+
+//Start Server 
+app.listen(httpPort, function(){
+	console.log('HTTP Server: http://localhost:'+ httpPort)
+});
