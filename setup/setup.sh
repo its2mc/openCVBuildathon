49812be2751d295f4b5db0a301851b97372bde6b
its2mc <its2uraps@msn.com>
2016-06-10 17:10:41 +0900

    Some edits to the setup script

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,6 +1,6 @@
 sudo apt-get update && 
-sudo apt-get install libopencv-dev python-opencv build-essential wget && 
+sudo apt-get install -y libopencv-dev python-opencv python-dev cmake build-essential python-setuptools wget libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev yasm libopencore-amrnb-dev libopencore-amrwb-dev libv4l-dev libxine2-dev&& 
 wget https://bootstrap.pypa.io/get-pip.py &&
 sudo python get-pip.py &&
 echo "Please ignore the errors on twisted installation\n"&&
-pip install numpy autobahn twisted 
+sudo pip install numpy autobahn twisted 
